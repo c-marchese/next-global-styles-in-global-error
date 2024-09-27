b1898d42d7cf59320b72770fe049d819c5745ace
c.marchese <c.marchese@reply.it>
2024-09-27 12:40:29 +0200

    global styles not present in global error page

---
+++ app/global-error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function GlobalError() {
+  return (
+    <html>
+      <body>
+        <h2>Error!</h2>
+      </body>
+    </html>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
+import { PHASE_PRODUCTION_BUILD } from "next/dist/shared/lib/constants";
+
+import "./global.css";
+
 export default function RootLayout({ children }) {
+  if (process.env.NEXT_PHASE !== PHASE_PRODUCTION_BUILD) {
+    throw new Error("Error");
+  }
+
   return (
     <html>
       <head />
